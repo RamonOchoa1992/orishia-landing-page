37f36a7025fd5397308eeb3767d77cb08ca98573
Ramon Ochoa Guerra <rochoaw10@gmail.com>
2026-02-23 14:49:55 -0500

    feat: Change layout config

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
           src='https://statics.orishiacore.com/connet/webchat/production/widget.js'
           strategy='afterInteractive'
           // @ts-expect-error: 'config' is a custom property required by Helpia widget
-          config='eyJjb25maWdJZCI6InI0cGEtLWVhNHBxeHVhdSIsIndzVG9rZW4iOiJxTU53QVEwVlFYeXlpekJxeXZJcnpodWdwaHZSUUg0Qko0d1Q2YXJjQVpMdE0iLCJiYWNrU2VydmVyIjoiaHR0cHM6Ly9iYWNrZW5kLmNvbm5ldC5vcmlzaGlhY29yZS5jb20iLCJ3c1NlcnZlciI6IndzczovL3dzLmJhY2tlbmQuY29ubmV0Lm9yaXNoaWFjb3JlLmNvbS9jaGFubmVscyIsImVuYWJsZVJlY2FwdGNoYSI6ZmFsc2V9'
+          config='eyJjb25maWdJZCI6Ii1zaDUtd2k3LWtjd2tkdSIsIndzVG9rZW4iOiJoZUVaLXI5cThrOXRpUnFpemJqRmdxRDVUMU9wTEp3enJBSDl3WFJhTkppRnYiLCJiYWNrU2VydmVyIjoiaHR0cHM6Ly9iYWNrZW5kLmNvbm5ldC5vcmlzaGlhY29yZS5jb20iLCJ3c1NlcnZlciI6IndzczovL3dzLmJhY2tlbmQuY29ubmV0Lm9yaXNoaWFjb3JlLmNvbS9jaGFubmVscyIsImVuYWJsZVJlY2FwdGNoYSI6ZmFsc2V9'
         />
       </body>
     </html>
